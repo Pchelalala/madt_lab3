73ecc950487de7442df772caeae7b11d3a2abe28
Pchela <lyudmilalittlepchela@gmail.com>
2022-11-07 11:04:43 +0200

    change appbar title

--- lib/app.dart
+++ lib/app.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Scientific Calculator',
+      title: 'MADT-3',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
